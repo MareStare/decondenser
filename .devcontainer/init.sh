dbfd6a5a9bdb11ba55401c374143f66217a6306f
MareStare <mare.stare.official@gmail.com>
2025-06-09 16:50:39 +0000

    Include workspace root

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../scripts/utils/lib.sh"
 
 # Start the container even if installation fails
-step npm install || true
+step npm install --workspaces --include-workspace-root || true
