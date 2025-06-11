14bddfb5d1de395e137d15edb42e91c6430fc363
MareStare <mare.stare.official@gmail.com>
2025-06-11 21:16:22 +0000

    Finish tokenizer

--- decondenser/src/tokenize/cursor.rs
+++ decondenser/src/tokenize/cursor.rs
@@ -1,15 +1,14 @@
-use super::{TokenKind, TokenTree};
 use crate::error::{Error, Result};
 use std::str::Chars;
 
-pub(crate) struct Cursor<'a> {
+pub(super) struct Cursor<'a> {
     /// Total number of bytes in the input.
     bytes: u32,
     chars: Chars<'a>,
 }
 
 impl<'a> Cursor<'a> {
-    pub(crate) fn new(input: &'a str) -> Result<Cursor<'a>> {
+    pub(super) fn new(input: &'a str) -> Result<Cursor<'a>> {
         let len = input.len();
 
         let Ok(bytes) = len.try_into() else {
@@ -26,22 +25,27 @@ impl<'a> Cursor<'a> {
         })
     }
 
-    pub(crate) fn peek(&self) -> Option<char> {
+    pub(super) fn next(&mut self) -> Option<char> {
+        self.chars.next()
+    }
+
+    pub(super) fn peek(&self) -> Option<char> {
         self.chars.clone().next()
     }
 
-    pub(crate) fn peek_str(&self) -> &str {
-        self.chars.as_str()
-    }
-
-    pub(crate) fn offset(&self) -> u32 {
+    pub(super) fn byte_offset(&self) -> u32 {
         let remaining = self.chars.as_str().len();
 
         // We are sure `remaining` is within the limit due to the check in `new`
         self.bytes - remaining as u32
     }
 
-    pub(crate) fn next(&mut self) -> Option<char> {
-        self.chars.next()
+    pub(super) fn strip_prefix(&mut self, prefix: &str) -> Option<u32> {
+        let stripped = self.chars.as_str().strip_prefix(prefix)?;
+
+        let start = self.byte_offset();
+        self.chars = stripped.chars();
+
+        Some(start)
     }
 }
--- decondenser/src/tokenize/mod.rs
+++ decondenser/src/tokenize/mod.rs
@@ -1,117 +1,176 @@
 mod cursor;
 
+use crate::{EscapeSpec, GroupSpec, LanguageSpec, QuoteSpec, Str};
+
 use crate::error::Result;
 use cursor::Cursor;
 use std::borrow::Cow;
 use std::marker::PhantomData;
+use std::mem;
 
-type Str<'a> = Cow<'a, str>;
-
-struct TokenTree<'a> {
-    kind: TokenKind<'a>,
-
-    /// Index of the token start in the input string. We assume the input fits
-    /// into the range of a `u32`. Hopefully no one needs to decondense a string
-    /// longer than 4GB.
-    start: u32,
-
-    /// Capture the lifetime of the input string since the [`Token`] technically
-    /// references a substring in the input string, so the [`Token`] makes no
-    /// sense without the input string, and mixing up the input string with some
-    /// other string would be a bug.
-    _phantom: PhantomData<&'a ()>,
+#[derive(Debug)]
+pub(crate) enum TokenTree {
+    Whitespace { start: u32 },
+    Group(Group),
+    Quoted(Quoted),
+    Raw { start: u32 },
+    Punct { start: u32 },
 }
 
-impl<'a> TokenTree<'a> {
-    fn new(kind: TokenKind<'a>, start: u32) -> Self {
-        TokenTree {
-            kind,
-            start,
-            _phantom: PhantomData,
-        }
-    }
+#[derive(Debug)]
+pub(crate) struct Quoted {
+    /// Offset of the opening quote
+    pub(crate) opening: u32,
+
+    /// Offset of the content, which is also the offset of the character that
+    /// follows the opening quote. Will be equal to `closing` if the content
+    /// is empty.
+    pub(crate) content: Vec<QuotedContent>,
+
+    /// Offset of the closing quote. Can be `None` if the quotes are not closed
+    /// (probably a malformed input).
+    pub(crate) closing: Option<u32>,
 }
 
-enum TokenKind<'a> {
-    Whitespace,
-    Nesting(Vec<TokenTree<'a>>),
-    Quoted,
+#[derive(Debug)]
+pub(crate) enum QuotedContent {
+    Raw { start: u32 },
+    Escape { start: u32 },
 }
 
-struct Nesting<'a> {
-    tokens: Box<[TokenTree<'a>]>,
+#[derive(Debug)]
+pub(crate) struct Group {
+    /// The start offset of the opening delimiter
+    pub(crate) opening: u32,
 
-    /// The start offset of the end delimiter.
-    end: u32,
+    /// The first token contains the start offset of the content of the group,
+    /// unless the group is empty.
+    pub(crate) content: Vec<TokenTree>,
+
+    /// Offset of the closing delimiter. Can be `None` if the group is not closed
+    /// (probably a malformed input).
+    pub(crate) closing: Option<u32>,
 }
 
 pub(crate) struct TokenizeParams<'a> {
-    input: &'a str,
-    escape_char: char,
-    nestings: &'a [(Str<'a>, Str<'a>)],
-    quotes: &'a [(Str<'a>, Str<'a>)],
+    pub(crate) input: &'a str,
+    pub(crate) lang: &'a LanguageSpec<'a>,
 }
 
-pub(crate) fn tokenize(params: TokenizeParams<'_>) -> Result<Vec<TokenTree<'_>>> {
+pub(crate) fn tokenize(params: TokenizeParams<'_>) -> Result<Vec<TokenTree>> {
     let mut lexer = Lexer {
-        escape_char: params.escape_char,
-        nestings: params.nestings,
-        quotes: params.quotes,
+        lang: params.lang,
         cursor: Cursor::new(params.input)?,
         output: Vec::new(),
     };
 
-    Ok(lexer.tokenize())
+    lexer.tokenize(None);
+
+    Ok(lexer.output)
 }
 
 struct Lexer<'a> {
-    escape_char: char,
-    nestings: &'a [(Str<'a>, Str<'a>)],
-    quotes: &'a [(Str<'a>, Str<'a>)],
+    lang: &'a LanguageSpec<'a>,
     cursor: Cursor<'a>,
-    output: Vec<TokenTree<'a>>,
+    output: Vec<TokenTree>,
 }
 
 impl<'a> Lexer<'a> {
-    fn tokenize(mut self) -> Vec<TokenTree<'a>> {
-        while let Some(char) = self.cursor.peek() {
+    fn tokenize(&mut self, terminator: Option<&Str<'a>>) -> Option<u32> {
+        while self.cursor.peek().is_some() {
             self.whitespace();
 
-            let remainder = self.cursor.peek_str();
-
-            let delim = self
-                .nestings
-                .iter()
-                .find(|(start, end)| remainder.starts_with(start.as_ref()));
-
-            if let Some(delim) = delim {
-                let tokens = self.tokenize_nesting(delim);
+            if let Some(start) = terminator.and_then(|term| self.cursor.strip_prefix(term)) {
+                return Some(start);
             }
-        }
 
-        self.output
-    }
+            let group_spec = self.lang.groups.iter().find_map(|group_spec| {
+                Some((self.cursor.strip_prefix(&group_spec.opening)?, group_spec))
+            });
 
-    fn tokenize_nesting(&mut self, (start, end): &(Str<'a>, Str<'a>)) -> Nesting<'a> {
-        // Consume the start delimiter
-        for _ in start.chars() {
+            if let Some((opening, group_spec)) = group_spec {
+                self.tokenize_group(opening, group_spec);
+                continue;
+            }
+
+            let quote_spec = self.lang.quotes.iter().find_map(|quote_spec| {
+                Some((self.cursor.strip_prefix(&quote_spec.opening)?, quote_spec))
+            });
+
+            if let Some((opening, quote_spec)) = quote_spec {
+                self.tokenize_quoted(opening, quote_spec);
+                continue;
+            }
+
+            let punct = self
+                .lang
+                .puncts
+                .iter()
+                .find_map(|punct| self.cursor.strip_prefix(punct));
+
+            if let Some(start) = punct {
+                self.output.push(TokenTree::Punct { start });
+                continue;
+            }
+
+            if !matches!(self.output.last(), Some(TokenTree::Raw { .. })) {
+                let start = self.cursor.byte_offset();
+                self.output.push(TokenTree::Raw { start });
+            }
+
             self.cursor.next();
         }
 
-        let mut output = Vec::new();
+        None
+    }
 
-        while let Some(char) = self.cursor.peek() {
-            if char == end.chars().next().unwrap() {
-                // Consume the end delimiter
-                for _ in end.chars() {
-                    self.cursor.next();
-                }
-                break;
-            } else {
-                // Recursively tokenize the inner content
-                self.whitespace();
+    fn tokenize_group(&mut self, opening: u32, group_spec: &GroupSpec<'a>) {
+        let prev = mem::take(&mut self.output);
+
+        let closing = self.tokenize(Some(&group_spec.closing));
+
+        let group = Group {
+            opening,
+            content: mem::replace(&mut self.output, prev).into(),
+            closing,
+        };
+
+        self.output.push(TokenTree::Group(group));
+    }
+
+    fn tokenize_quoted(&mut self, opening: u32, quote_spec: &QuoteSpec<'a>) {
+        let mut content = vec![];
+
+        let closing = loop {
+            let escape = quote_spec
+                .escapes
+                .iter()
+                .find_map(|escape| self.cursor.strip_prefix(&escape.escaped));
+
+            if let Some(start) = escape {
+                content.push(QuotedContent::Escape { start });
+                continue;
             }
-        }
+
+            if let Some(closing) = self.cursor.strip_prefix(&quote_spec.closing) {
+                break Some(closing);
+            }
+
+            if !matches!(content.last(), Some(QuotedContent::Raw { .. })) {
+                let start = self.cursor.byte_offset();
+                content.push(QuotedContent::Raw { start });
+            }
+
+            self.cursor.next();
+        };
+
+        let quoted = Quoted {
+            opening,
+            content,
+            closing,
+        };
+
+        self.output.push(TokenTree::Quoted(quoted));
     }
 
     fn whitespace(&mut self) {
@@ -121,7 +180,8 @@ impl<'a> Lexer<'a> {
             }
         }
 
-        self.push_token(TokenKind::Whitespace);
+        let start = self.cursor.byte_offset();
+        self.output.push(TokenTree::Whitespace { start });
 
         while let Some(char) = self.cursor.peek() {
             if !char.is_whitespace() {
@@ -129,10 +189,6 @@ impl<'a> Lexer<'a> {
             }
             self.cursor.next();
         }
-    }
-
-    fn push_token(&mut self, kind: TokenKind<'a>) {
-        self.output.push(TokenTree::new(kind, self.cursor.offset()));
     }
 }
 #[derive(Debug, Clone, Copy, PartialEq)]
